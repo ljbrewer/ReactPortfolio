5cb49d08c505f5dd586155193a92da180837190b
ljbrewer <ljbrewer.lb@gmail.com>
2021-11-20 17:19:55 -0800

    Updates

--- src/components/pages/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return(
-    <div>
-  <h2>A Passion for Coding </h2> 
-  <h3>Made with <span role="img" aria-label="heart">❤️</span> and React</h3>
-    </div>
-  )}
